e78f43b52a88aabebf4f2601eb17bbb734520b81
zoy <zoy@MacBook-Air-2012.local>
2020-03-28 14:54:05 +0900

    初期化SQL文の修正 #6

--- db/initdb.d/test.sql
+++
@@ -1,8 +0,0 @@
-create table IF not exists `test_table`
-(
- `id`               INT(20) AUTO_INCREMENT,
- `name`             VARCHAR(20) NOT NULL,
- `created_at`       Datetime DEFAULT NULL,
- `updated_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
-)
